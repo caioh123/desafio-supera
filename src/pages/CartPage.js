ed0e275a22eb4cf10134d1d1119c7fbcb8330a65
caioh123 <caionwebdev@gmail.com>
2021-08-26 12:24:21 -0300

    finishing cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,16 +1,56 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
 import { Header } from '../components/Header'
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, EvilIcons, AntDesign } from '@expo/vector-icons';
+import {data} from '../products'
+import {useNavigation} from '@react-navigation/native'
+
 
 export const CartPage = () => {
-    return (
-        <View style={styles.container}>
-            <Header />
-            <View style={styles.cardContainer}>
 
+    const navigation = useNavigation()
+
+
+    return (
+    <>
+        <Header />
+        <View style={styles.container}>
+            <View style={styles.cardContainer}>
+                <TouchableOpacity onPress={() => navigation.navigate("HomePage")} style={styles.cardHeader}>
+                    <Ionicons name="arrow-back-circle-outline" size={40} color="black" />
+                    <Text style={styles.cardBack}>Voltar</Text>
+                </TouchableOpacity>
+                    <View style={styles.cardDesc}>
+                        <Text style={styles.cardDescText}></Text>
+                        <Text style={styles.cardDescText}>Produto</Text>
+                        <Text style={styles.cardDescText}>QTD</Text>
+                    </View>
+                    <View style={styles.cardMain}>
+                        <Image style={styles.gameImg} source={data[4].image} />
+                        <View style={styles.gameContainer} alignItems={"center"}>
+                            <Text style={styles.gameName}>{data[0].name}</Text>
+                            <Text style={styles.gamePrice}>R$ {data[0].price}</Text>
+                        </View>
+                        <View style={styles.cardQty}>
+                            <AntDesign name="minuscircleo" size={20} color="rgb(173,58,16)" />
+                            <Text style={{fontSize: 20, paddingLeft: 5}}>1</Text>
+                            <EvilIcons name="plus" size={30} color="rgb(173,58,16)" />
+                        </View>
+                    </View>
+                    <View style={styles.priceContainer}>
+                        <Text style={styles.priceDesc}>FRETE</Text>
+                        <Text style={styles.priceDesc}>TOTAL</Text>
+                    </View>
+                    <View style={styles.priceContainer}>
+                        <Text style={styles.priceVal}>R$ 50,00</Text>
+                        <Text style={styles.priceVal}>R$ {data[0].price}</Text>
+                    </View>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.textBtn}>ADICIONAR AO CARRINHO</Text>
+                    </TouchableOpacity>
             </View>
         </View>
+        </>
     )
 }
 
@@ -23,12 +63,93 @@ const styles = StyleSheet.create({
     cardContainer: {
         flexDirection: "column",
         borderRadius: 15,
-        justifyContent: "center",
         height: 400,
         backgroundColor: "white",
         marginTop: 20,
         width: 350,
         padding: 20,
-        marginBottom: 30,
-    }
+    },
+    cardHeader: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    cardBack: {
+        fontSize: 21,
+        color: "black",
+        paddingLeft: 10,
+    },
+    cardDesc: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingTop: 20,
+    },
+    cardDescText: {
+        fontSize: 18,
+        color: "rgb(153,153,153)",
+    },
+    cardMain: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingTop: 20,
+        justifyContent: "space-between",
+        height: 150,
+        borderBottomColor: "rgb(80,80,80)",
+        paddingBottom: 40,
+        borderBottomWidth: 2
+    },
+    gameImg: {
+        width: 60,
+        height: 100,
+    },
+    cardQty: {
+        flexDirection: 'row',
+        width: 70,
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 10,
+    },
+    gameName: {
+        color: "black",
+        fontSize: 16,
+
+    },
+    gamePrice: {
+        fontWeight: 'bold',
+        color: "black",
+        fontSize: 16,
+
+    },
+    priceContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 5,
+    },
+    priceDesc: {
+        color: "black",
+        fontSize: 18
+    },
+    priceVal: {
+        fontWeight: "bold",
+        color: "black",
+        fontSize: 22
+    },
+    btn: {
+        backgroundColor: "rgb(173,58,16)",
+        color: "#fff",
+        borderRadius: 10,
+        marginTop: 20,
+        alignItems: "center",
+        height: 50,
+        flexDirection: "row",
+        justifyContent: "center",
+
+    },
+    textBtn: {
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        paddingRight: 10,
+        
+    },
 })
