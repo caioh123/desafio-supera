14fa09ebde566db5b4227f202929fad0cbc5dc77
caioh123 <caionwebdev@gmail.com>
2021-08-29 13:46:22 -0300

    feature: sorting by popularity and price

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,48 +11,48 @@ import {
 import { GameCard } from "../components/GameCard";
 import { Header } from "../components/Header";
 import { useGames } from "../context/gameContext";
+import { Picker } from "@react-native-picker/picker";
 
 export const HomePage = ({ navigation }) => {
   const { games } = useGames();
-  const [filteredData, setFilteredData] = useState([]);
+  const [selectedFilter, setSelectedFilter] = useState("price");
 
   const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    searchFilter(search);
-  }, [search]);
-
-  const searchFilter = (text) => {
-    if (text) {
-      const newData = games.filter((item) => {
-        // const itemData = item.name ? item.name.toUpperCase() : "".toUpperCase()
-
-        // const textData = text.toUpperCase()
-        // return itemData.indexOf(textData) >- 1
-        console.log(text);
-      });
-      setFilteredData(newData);
-      setSearch(text);
+  const ItemsToShow = games.filter(() => {
+    if (selectedFilter === "price") {
+      return games.sort((a, b) => b.price - a.price);
     } else {
-      console.log(text);
-      setFilteredData(games);
-      setSearch(text);
+      return games.sort((a, b) => b.score - a.score);
     }
-  };
+  });
 
   return (
     <>
       <Header />
       <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            value={search}
-            onChangeText={setSearch}
-            textAlign={"center"}
-            placeholder="Buscar seu jogo favorito"
-            style={styles.input}
-          />
+        <View style={styles.inputs}>
+          <Picker
+            style={[styles.inputContainer, { width: "40%" }]}
+            selectedValue={selectedFilter}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedFilter(itemValue)
+            }
+          >
+            <Picker.Item label="Popularidade" value="pop" />
+            <Picker.Item label="Preço" value="price" />
+          </Picker>
+          <View style={styles.inputContainer}>
+            <TextInput
+              value={search}
+              onChangeText={setSearch}
+              textAlign={"center"}
+              placeholder="Buscar seu jogo favorito"
+              style={styles.input}
+            />
+          </View>
         </View>
+
         <View>
           {/* 
             <FlatList 
@@ -65,11 +65,13 @@ export const HomePage = ({ navigation }) => {
             /> */}
 
           <ScrollView>
-            {games
-              .filter((dataFiltered) => dataFiltered.name.includes(search))
-              .map((item) => (
-                <GameCard item={item} />
-              ))}
+            {search
+              ? games
+                  .filter((dataFiltered) => dataFiltered.name.includes(search))
+                  .map((item, index) => <GameCard key={index} item={item} />)
+              : ItemsToShow.map((item, index) => (
+                  <GameCard key={index} item={item} />
+                ))}
           </ScrollView>
         </View>
       </View>
@@ -81,10 +83,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "rgb(25,25,32)",
   },
   input: {
     color: "rgba(0, 0, 0, 0.5)",
     fontSize: 14,
+  },
+  inputs: {
+    width: "100%",
+    justifyContent: "space-between",
+    flexDirection: "row",
   },
   inputContainer: {
     backgroundColor: "white",
@@ -92,7 +100,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 6,
     height: 50,
-    width: "55%",
+    width: "50%",
     paddingTop: 8,
   },
 });
